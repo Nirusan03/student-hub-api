db3be52d41444f6f135bc368a3608dff717a877f
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-24 02:22:40 +0530

    [1] Springboot project setup completed

---
+++ src/main/java/com/example/student_hub_api/StudentHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_hub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/student_hub_api/StudentHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_hub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
