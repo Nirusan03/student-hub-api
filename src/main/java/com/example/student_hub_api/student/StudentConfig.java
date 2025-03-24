076033a5f3d10b6df20f5c178367c7a0211ce183
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-24 17:50:40 +0530

    [11] Student config file added

---
+++ src/main/java/com/example/student_hub_api/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.student_hub_api.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student nirusan = new Student(
+                    "Nirusan Hariharan",
+                    "nirusan.harihara350@gmail.com",
+                    LocalDate.of(2003, Month.OCTOBER, 9),
+                    22
+            );
+
+            Student malith = new Student(
+                    "Malith Lekamage",
+                    "malith.lekamge@gmail.com",
+                    LocalDate.of(2002, Month.FEBRUARY, 10),
+                    23
+            );
+
+            repository.saveAll(List.of(nirusan, malith));
+        };
+    }
+}
